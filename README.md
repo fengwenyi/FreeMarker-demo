b5522ac69c051e693f35e630e2e4c537f2f4f91d
fengwenyi <xfsy_2015@163.com>
2018-06-14 22:30:00 +0800

    变量定义、复制、运算、比较、逻辑、对象、集合

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 # FreeMarker
 
-1、取值
+![Hello World](https://upload-images.jianshu.io/upload_images/5805596-ae06fd66387de240.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
 
-2、运算，集合
 
-3、高级技巧
+1、从取值开始学习FreeMarker
+
+2、变量定义、赋值、简单运算、比较、逻辑运算
+
+3、if switch
+
+4、对象、集合
+
+5、高级技巧
+
+6、FreeMarker 远没有看上去那么简单，所有要在实际中去学习、掌握它，并做好笔记。
+
+不巧的是，在同事的劝说下，
+
+已准备学习Vue.js，所以，下次更新，就应该是Vue.js的内容。
+
+FreeMarker且行且珍惜吧。
+
+![目录](https://upload-images.jianshu.io/upload_images/5805596-438c1aaf474ead08.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
 
 测试代码，请看 `example` !
--- example/src/main/java/com/fengwenyi/demo/freemarker/example/controller/ValueController.java
+++ example/src/main/java/com/fengwenyi/demo/freemarker/example/controller/ValueController.java
@@ -1,11 +1,12 @@
 package com.fengwenyi.demo.freemarker.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Date;
+import java.util.*;
 
 /**
  * @author Wenyi Feng
@@ -27,6 +28,42 @@ public class ValueController {
         mv.addObject("dateUtilVar", new Date());
         mv.addObject("dateSqlVar", new java.sql.Date(new Date().getTime()));
         mv.addObject("nullVar", null);
+        return mv;
+    }
+
+    @RequestMapping("/calculate")
+    public String calculate(ModelMap map){
+
+        map.put("x", 34);
+        map.put("y", 12);
+        map.put("z", -12.5);
+        map.put("w", 11.4);
+        map.put("T", true);
+        map.put("F", false);
+
+        return "/calculate";
+    }
+
+    @RequestMapping("/obj")
+    public ModelAndView obj() {
+        ModelAndView mv = new ModelAndView("obj");
+        User user = new User(1L, "张三", 20);
+        mv.addObject("user", user);
+
+        List<User> userList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            userList.add(new User(Long.valueOf(i), "张" + i, (int) (Math.random() * 100)));
+        }
+        mv.addObject("list", userList);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        map.put("key3", "value3");
+        map.put("key4", "value4");
+        map.put("key5", "value5");
+        mv.addObject("map", map);
+
         return mv;
     }
 
